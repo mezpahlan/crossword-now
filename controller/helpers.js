c65ad2b6e9c52e91f80da9813dc66a57bf3ecafe
Mez Pahlan <mez.pahlan@gmail.com>
2015-09-27 20:48:18 +0100

    Helper methods for creating a hash and unhashing of the crossword id from the guardian.

--- controller/helpers.js
+++ controller/helpers.js
@@ -4,3 +4,26 @@
 exports.randomFromArray = function (p) {
     return Math.floor(Math.random() * (p.length - 1));
 };
+
+// Return a hash of a string crossword id
+exports.hash = function (p) {
+    var num, direction;
+    num = /\d+/.exec(p)[0];
+    direction = /[^-0-9]/.exec(p)[0].toUpperCase();
+
+    return num.concat(direction);
+};
+
+// Returns a crossword id from a hash
+exports.unHash = function (p) {
+    var num, hash, direction;
+    num = /\d+/.exec(p)[0];
+    hash = /\D/.exec(p)[0];
+    if (hash === 'A') {
+        direction = 'across';
+    } else if (hash === 'D') {
+        direction = 'down';
+    }
+
+    return num.concat('-').concat(direction);
+};
