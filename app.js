7aed86375fc2a27f3d729692d985fde2968a2007
Mez Pahlan <mez.pahlan@gmail.com>
2016-01-04 13:30:19 +0000

    Sync to master DB on startup to avoid losing documents when pushing updates.

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 'use strict';
 
 var cfenv = require('cfenv');
-var appEnv = cfenv.getAppEnv();
-var routes = require('./routes/index');
+var Promise = require('bluebird');
+var PouchDB = require('pouchdb');
 var express = require('express');
+var routes = require('./routes/index');
 var app = express();
+var appEnv;
+var vcap;
+
+try { vcap = require('./.vcap.json'); } catch (e) { }
+
+if (vcap === null) {
+    // Running on CF
+    appEnv = cfenv.getAppEnv();
+} else {
+    // Running locally
+    appEnv = cfenv.getAppEnv({vcap: vcap});
+}
+
+var couchUri = appEnv.getServiceURL('crossword-cloudant').concat('crosswords');
+var localDb = new PouchDB('crosswords');
+var remoteDb = new PouchDB(couchUri);
 
 app.use('/', routes);
 
-var server = app.listen(appEnv.port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+var startServer = function () {
+    return new Promise(function(resolve, reject) {
+        let server = app.listen(appEnv.port, appEnv.bind, function() {
+                                    console.log('Server starting on ' + appEnv.url);
 
-    console.log('Crossword app listening at http://%s:%s', host, port);
-});
+                                });
+        resolve(server);
+    });
+};
+
+// First replicate from remote
+// Then set up server
+remoteDb.sync(localDb)
+        .then(result => console.log(result))
+        .then(() => startServer())
+        .catch(error => console.log(error));
