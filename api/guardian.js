b97c905357f9719ebeb471a4b56900aeed957230
Mez Pahlan <mez.pahlan@gmail.com>
2016-02-01 22:54:23 +0000

    Adding documentation to class

--- api/guardian.js
+++ api/guardian.js
@@ -4,9 +4,18 @@ var rp = require('request-promise');
 var cheerio = require('cheerio');
 var Helper = require('../controller/helpers');
 
+/**
+ * Methods for accessing crosswords provided by The Guardian.
+ */
 class Guardian {
     constructor () {}
 
+    /**
+     * Scrapes the website for crossword data.
+     *
+     * @param {string} id - The unique crossword identification.
+     * @returns {Promise} - Resolves with a JSON document representing the crossword or rejects with a retry of the next available id.
+     */
     static scrape (id) {
 
         const BASE_URL = 'http://www.theguardian.com/crosswords/';
