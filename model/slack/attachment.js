959faeafda8da6d41ef3e84ca4ef87c33ba91d8e
Mez Pahlan <mez.pahlan@gmail.com>
2016-02-01 22:59:44 +0000

    Adding documentation to Attachment class

--- model/slack/attachment.js
+++ model/slack/attachment.js
@@ -1,6 +1,16 @@
 'use strict';
 
+/**
+ * An Attachment is an optional extra to the response sent back to Slack.
+ * It should contain enriching information to the main response text.
+ */
 class Attachment {
+    /**
+     * Instantiate a new Attachment.
+     *
+     * @param {string} title - The title of the attachment.
+     * @param {array} fields - Array of [Fields]{@link Field}.
+     */
     constructor (title, fields) {
         let fallback = title + (!fields || fields.length === 0 ? '' : ' - ' + fields.reduce(this._constructFallback, ''));
 
@@ -10,6 +20,15 @@ class Attachment {
         this.fallback =  fallback;
     }
 
+    /**
+     * Creates a single lined fallback text for readers that cannot display Fields.
+     *
+     * @param {string} prev - The value previously returned in the last invocation of the callback, or initialValue, if supplied.
+     * @param {string} curr - The current element being processed in the array.
+     * @param {number} index - The index of the current element being processed in the array.
+     * @param {array} array - The array [reduce]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce} was called upon.
+     * @return {string} The fallback text.
+     */
     _constructFallback (prev, curr, index, array) {
         prev += curr.title + ': ' + curr.value + ', ';
         return (index === array.length - 1) ? prev.slice(0, -2) : prev;
