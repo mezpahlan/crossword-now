09570d5087b9438b96689ee7fede7571d66220bb
Mez Pahlan <mez.pahlan@gmail.com>
2016-02-27 23:08:36 +0000

    Adding extra cases for testing

--- test/doubles/api/guardian.js
+++ test/doubles/api/guardian.js
@@ -5,16 +5,23 @@ var Bluebird = require('bluebird');
 
 function guardianApiDouble (url) {
     let promise;
+    let response;
 
     switch (url) {
+        case (url.match(/quick\/14140/) || {}).input:
+            response = fs.readFileSync('test/data/quick.html', 'utf8');
+            promise = Bluebird.resolve(response.trim());
+            break;
         case (url.match(/cryptic\/26670/) || {}).input:
             promise = Bluebird.reject('Test fetching \'next\' id.');
-
             break;
         case (url.match(/cryptic\/26671/) || {}).input:
-            let response = fs.readFileSync('test/data/cryptic.html', 'utf8');
+            response = fs.readFileSync('test/data/cryptic.html', 'utf8');
             promise = Bluebird.resolve(response.trim());
-
+            break;
+        case (url.match(/cryptic\/26672/) || {}).input:
+            response = fs.readFileSync('test/data/cryptic.html', 'utf8');
+            promise = Bluebird.resolve(response.trim());
             break;
         default:
             promise = Bluebird.reject('Testing error. Case needs to be defined.');
